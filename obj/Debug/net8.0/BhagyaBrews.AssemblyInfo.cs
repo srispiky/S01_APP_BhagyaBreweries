c5c0d9751c9baf40e628d4d21958faf678e23e1f
Srinivasa_Kumar <srispiky@gmail.com>
2025-08-24 04:12:42 +0530

    Updated Home page

--- obj/Debug/net8.0/BhagyaBrews.AssemblyInfo.cs
+++ obj/Debug/net8.0/BhagyaBrews.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BhagyaBrews")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62596ae7af905e5846c300d85c7ae0500747f67b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BhagyaBrews")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BhagyaBrews")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
