06a721fe9ee366ec2d31ff980a3e7334e502bb34
Srinivasa_Kumar <srispiky@gmail.com>
2025-08-24 04:12:42 +0530

    Updated Home page

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Tell ASP.NET Core to look for default files like index.html
+app.UseDefaultFiles(new DefaultFilesOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory())),
+    RequestPath = ""
+});
+
+// Enable serving static files from the project root
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory())),
+    RequestPath = ""
+});
 
 app.Run();
